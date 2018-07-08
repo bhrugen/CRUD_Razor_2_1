6d809c2879eb556e36b041602ddaacb06721c46e
Ben Spark <bhrugen1990@gmail.com>
2018-07-08 02:48:53 -0500

    Delete Book

--- CRUD_Razor_2_1/Pages/BookList/Index.cshtml.cs
+++ CRUD_Razor_2_1/Pages/BookList/Index.cshtml.cs
@@ -28,5 +28,17 @@ namespace CRUD_Razor_2_1.Pages.BookList
         {
             Books = await _db.Books.ToListAsync();
         }
+
+        public async Task<IActionResult> OnPostDelete(int id)
+        {
+            var book = await _db.Books.FindAsync(id);
+            _db.Books.Remove(book);
+            await _db.SaveChangesAsync();
+
+            Message = "Book deleted successully.";
+
+            return RedirectToPage("Index");
+        }
+
     }
 }
