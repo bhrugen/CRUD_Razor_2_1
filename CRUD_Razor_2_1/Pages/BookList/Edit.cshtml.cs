f2baa060205864407bfd0a10e7715fad5df3a06a
Ben Spark <bhrugen1990@gmail.com>
2018-07-08 03:01:15 -0500

    Edit Page Model

--- CRUD_Razor_2_1/Pages/BookList/Edit.cshtml.cs
+++ CRUD_Razor_2_1/Pages/BookList/Edit.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CRUD_Razor_2_1.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,9 +10,40 @@ namespace CRUD_Razor_2_1.Pages.BookList
 {
     public class EditModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly ApplicationDbContext _db;
 
+        public EditModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [BindProperty]
+        public Book Book { get; set; }
+
+        [TempData]
+        public string Message { get; set; }
+
+        public void OnGet(int id)
+        {
+            Book = _db.Books.Find(id);
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if(ModelState.IsValid)
+            {
+                var BookFromDb = _db.Books.Find(Book.Id);
+                BookFromDb.Name = Book.Name;
+                BookFromDb.ISBN = Book.ISBN;
+                BookFromDb.Author = Book.Author;
+
+                await _db.SaveChangesAsync();
+                Message = "Book has been updated successfully";
+
+                return RedirectToPage("Index");
+            }
+
+            return RedirectToPage();
         }
     }
 }
