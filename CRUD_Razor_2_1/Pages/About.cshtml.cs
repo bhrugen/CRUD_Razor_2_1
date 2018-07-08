2bec242363bff51d647d24abe8ec8f7ed4d7f247
Ben Spark <bhrugen1990@gmail.com>
2018-07-08 00:27:48 -0500

    Add project files.

---
+++ CRUD_Razor_2_1/Pages/About.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CRUD_Razor_2_1.Pages
+{
+    public class AboutModel : PageModel
+    {
+        public string Message { get; set; }
+
+        public void OnGet()
+        {
+            Message = "Your application description page.";
+        }
+    }
+}
